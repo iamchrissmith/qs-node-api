fabf3abccf784bf534f0d960ef4e3a800117adca
rongxanh88 <rongxanh88@gmail.com>
2017-08-29 12:32:20 -0600

    commented out database queries for fetching foods with the meal

--- controllers/mealController.js
+++ controllers/mealController.js
@@ -1,16 +1,26 @@
-const Meal = require('../models/meal')
+const Meal       = require('../models/meal')
+
 const pry  = require('pryjs')
 
 exports.meals = (request, response) => {
   Meal.all()
     .then((meals) => {
-      eval(pry.it)
+      // Promise.all([meals.forEach(meal => Meal.foodsForMeal(meal))])
+      //   .then((foods) => {
+      //     eval(pry.it)
+      //   })
+
       response.json(meals);
     });
 };
 
 exports.meal_with_foods = (request, response) => {
+  // const mealID = request.params.id;
 
+  // Meal.withFoods(mealID)
+  //   .then((foods) => {
+  //     response.json(foods);
+  //   })
 };
 
 exports.add_food_to_meal = (request, response) => {
--- models/meal.js
+++ models/meal.js
@@ -6,12 +6,33 @@ const pry  = require('pryjs')
 class Meal {
   static all() {
     return database.raw(
-      'SELECT * FROM meals'
+      'SELECT id, name FROM meals'
     )
     .then((data) => {
       return data.rows;
     });
   };
+
+  // static withFoods(mealID) {
+  //   return database.raw(
+  //     'SELECT id, name, calories FROM foods' +
+  //     ' INNER JOIN meal_foods ON foods.id = meal_foods.food_id' +
+  //     ' WHERE meal_foods.meal_id = ?', mealID
+  //   ).then((data) => {
+  //     return data.rows;
+  //   });
+  // }
+
+  // static foodsForMeal(mealID) {
+  //   return database.raw(
+  //     'SELECT * FROM foods INNER JOIN meal_foods' +
+  //     ' ON foods.id = meal_foods.food_id WHERE meal_foods.meal_id = ?',
+  //     mealID
+  //   ).then((data) => {
+  //     return data.rows;
+  //   });
+  // }
 }
+
 
 module.exports = Meal
