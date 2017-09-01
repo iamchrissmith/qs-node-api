b5b98ec924c0e106cddb566bf96041143741bd70
rongxanh88 <rongxanh88@gmail.com>
2017-09-01 10:29:14 -0600

    remove pry import from meal.js

--- models/meal.js
+++ models/meal.js
@@ -1,7 +1,7 @@
 const environment   = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const database      = require('knex')(configuration);
-const pry = require('pryjs')
+
 class Meal {
 
   constructor(attrs) {
